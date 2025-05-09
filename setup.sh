ee6b446362fa59bd6f8f45b8703923d4dc4bfcd2
Aleksandr Ovchinin <alex.ovchinin@gmail.com>
2025-05-10 00:35:04 +0500

    Fix bash loop

--- setup.sh
+++ setup.sh
@@ -46,7 +46,7 @@ update_homepage() {
     mkdir "$APPS_DIR/homepage"
     ln -s "$SOURCE_DIR/apps/homepage/images" "$APPS_DIR/homepage/images"
     mkdir "$APPS_DIR/homepage/configs"
-    for source_file in "$SOURCE_DIR/homepage/configs"/*; do
+    for source_file in "$SOURCE_DIR/homepage/configs/*"; do
         local filename=$(basename "source_file")
         local target_path="$APPS_DIR/homepage/configs/$filename"
         log "Creating symlink for $filename"
